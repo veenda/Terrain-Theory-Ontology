b56271df010f4bcdcdbaef65518e6a5c2939f5a0
Veenda Putri Divo <divoveenda@gmail.com>
2025-12-19 16:15:11 +0700

    Fixing python code

--- api/index.py
+++ api/index.py
@@ -5,8 +5,6 @@ from datetime import datetime
 import pytz
 import os
 
-import os
-# Mendapatkan jalur absolut ke folder templates
 template_dir = os.path.abspath(os.path.join(os.path.dirname(__file__), '..', 'templates'))
 app = Flask(__name__, template_folder=template_dir)
 
